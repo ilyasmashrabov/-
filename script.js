56a3b83897b3219d98a4d93724698eabe8b71c70
ilyasmashrabov <82878620+ilyasmashrabov@users.noreply.github.com>
2021-05-04 13:07:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var second = +prompt('Введите число:');
+var min = second / 60;
+var hour = min / 60;
+var day = hour / 24;
+alert('Зайдите в консоль чтобы посмотреть (F12)');
+console.log(second + ' секунд');
+console.log(min + ' минут');
+console.log(hour + ' часов');
+console.log(day + ' дней');
