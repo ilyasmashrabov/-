5e03f67496cb96d8f4ba54fce0aefc7c60f9d303
ilyasmashrabov <82878620+ilyasmashrabov@users.noreply.github.com>
2021-05-10 12:45:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,102 @@
-var second = +prompt('Введите число:');
-var min = second / 60;
-var hour = min / 60;
-var day = hour / 24;
-alert('Зайдите в консоль чтобы посмотреть (F12)');
-console.log(second + ' секунд');
-console.log(min + ' минут');
-console.log(hour + ' часов');
-console.log(day + ' дней');
+// цикл
+// ++
+
+// var i = 0;
+// console.log(++i); // префиксная
+
+// console.log(i++); // постфиксная
+// console.log(i);
+
+
+//  = =============== while -========================
+// var i = 0;
+
+// while (i < 10) {
+//   console.log(i);
+//   i++;
+// }
+
+
+//  ================== do while =================
+// var i = 1000;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 10);
+
+// do {
+//   var x = +prompt('введите число');
+
+// } while (isNaN(x)){
+
+//   console.log(x);
+
+// }
+
+
+
+// ============== for ========================
+// for (var i = 0; i < 10; i++) {
+
+//   console.log(i);
+
+// }
+// console.log(i);
+
+// for (let i = 0; i < 10; i++) {
+
+//   console.log(i);
+
+// }
+
+// let a = +prompt('введите число');
+// let b;
+// условие ? выражение если условие true : выражение если условие false
+
+// a = !isNaN(a) ? a : 10;
+
+// if (!isNaN(a)) {
+//   a = a;
+// }else{
+//   a = 10;
+// }
+
+// for (let i = 0; i < a; i++) {
+
+//  console.log(i);
+
+// }
+
+
+
+
+// let answer = 1;
+// for (let i = 1; i <= 5; i++) {
+// answer = answer * i;
+  
+// }
+
+// console.log(answer);
+
+
+
+
+function ran(min,max) {
+    let num = Math.floor(Math.random() * (max + 1 - min) + min);
+    return num;
+}
+
+let minInt = +prompt('Введите минимальное число');
+let maxInt = +prompt('Введите максимальное число');
+
+for (let i = 0; i < 10; i++) {
+ 
+    let num1 = ran(minInt, maxInt);
+    let num2 = ran(minInt, maxInt);
+    let pr = +prompt(num1 + '+' + num2);
+    let answer = (num1 + num2) == pr ? 'Молодец' : 'Ошибка';
+    alert(num1 + '+' + num2 + ' = ' + (num1 + num2) + ' ваш ответ ' + pr + '->' + answer)
+
+    
+}
